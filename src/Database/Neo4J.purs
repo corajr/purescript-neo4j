fcd8b822ef4b092f7e6489b1a34f4d9e0bdd2abe
Cora Johnson-Roberson <me@corajr.com>
2016-07-21 23:32:15 -0400

    some docs

--- src/Database/Neo4J.purs
+++ src/Database/Neo4J.purs
@@ -38,6 +38,7 @@ instance showBasicAuth :: Show BasicAuth where
 instance eqBasicAuth :: Eq BasicAuth where
   eq = gEq
 
+-- | A `Query` returns an array of `a` records.
 newtype Query a = Query String
 
 instance eqQuery :: Eq (Query a) where
@@ -45,8 +46,10 @@ instance eqQuery :: Eq (Query a) where
 instance showQuery :: Show (Query a) where
   show (Query n) = n
 
+-- | Type of queries (transaction is held in a ReaderT environment).
 type InTransaction eff a = ReaderT Transaction (Aff (db :: DB | eff)) a
 
+-- | Query parameters.
 newtype Params = Params Foreign
 
 myForeignOpts :: Options
@@ -69,27 +72,42 @@ defaultConnectionOptions = ConnectionOptions
   { encrypted: false
   }
 
+-- | Server connection info; example:
+-- |
+-- | { url: "bolt://localhost",
+-- | , auth: mkAuth "username" "password"
+-- | , connectionOpts: defaultConnectionOptions { encrypted = true }
+-- | }
 data ConnectionInfo = ConnectionInfo
   { url :: String
   , auth :: BasicAuth
   , connectionOpts :: ConnectionOptions
   }
 
+-- | Make a `BasicAuth` object suitable for the Neo4J driver.
 mkAuth :: Username -> Password -> BasicAuth
 mkAuth user pass = unsafeFromForeign (mkAuth_ user pass)
 
+-- | Create a `Driver` from a `ConnectionInfo`. (See `withDriver` for a convenient
+-- | wrapper that closes the driver.)
 mkDriver :: forall eff. ConnectionInfo -> Eff (db :: DB | eff) Driver
 mkDriver (ConnectionInfo { url, auth, connectionOpts }) =
   let auth' = toForeignGeneric myForeignOpts auth
       connectionOpts' = toForeignGeneric myForeignOpts connectionOpts
   in runFn3 mkDriver_ url auth' connectionOpts'
 
+-- | Create a `Session` from a `Driver`. (See `withSession` for a convenient
+-- | wrapper that closes the session.)
 mkSession :: forall eff. Driver -> Eff (db :: DB | eff) Session
 mkSession driver = runFn1 mkSession_ driver
 
+-- | Turns a record such as `{name: "Arthur"}` into parameters for a query.
 mkParams :: forall a. a -> Params
 mkParams = Params <<< toForeign
 
+-- | Creates a `Driver` from the `ConnectionInfo`, runs the provided `Aff`
+-- | action, and finally closes the driver. One driver is suggested per server
+-- | over the lifecycle of the application.
 withDriver :: forall eff a.
               ConnectionInfo
            -> (Driver -> Aff (db :: DB | eff) a)
@@ -98,6 +116,8 @@ withDriver info f = do
   driver <- liftEff $ mkDriver info
   finally (f driver) $ closeDriver driver
 
+-- | Creates a session from the given `Driver`, runs the provided `Aff` action,
+-- | and finally closes the session.
 withSession :: forall eff a.
                Driver
             -> (Session -> Aff (db :: DB | eff) a)
@@ -106,6 +126,8 @@ withSession driver f = do
   session <- liftEff $ mkSession driver
   finally (f session) $ closeSession session
 
+-- | Runs queries in a transaction created from the given `Session`, commiting
+-- | changes at the end.
 withTransaction :: forall eff a.
                    Session
                 -> InTransaction eff a
@@ -114,6 +136,9 @@ withTransaction session f = do
   transaction <- liftEff $ runFn1 beginTransaction_ session
   finally (runReaderT f transaction) (liftEff $ runFn1 commitTransaction_ transaction)
 
+-- | Runs queries in a transaction created from the given session, then rolls
+-- | back. (Mostly useful for tests, when one doesn't want to write permanently
+-- | to the database.)
 withRollback :: forall eff a.
                 Session
              -> InTransaction eff a
@@ -122,17 +147,26 @@ withRollback session f = do
   transaction <- liftEff $ runFn1 beginTransaction_ session
   finally (runReaderT f transaction) (liftEff $ runFn1 rollbackTransaction_ transaction)
 
+-- | Run a query in the current transaction, discarding the result.
 execute :: forall eff. Query Unit -> Params -> InTransaction eff Unit
 execute q params = void (privateRunQuery_ q params)
 
+-- | Like `execute` with no parameters.
 execute' :: forall eff. Query Unit -> InTransaction eff Unit
 execute' q = execute q (mkParams {})
 
+-- | Run a query in the current transaction, converting the results to `Array a`
+-- | using the `IsForeign` instance or throwing an error if the conversion
+-- | cannot be performed.
+-- |
+-- | Parameters are given in the query string with curly braces, e.g.
+-- | `query (Query "MATCH (n:Person) WHERE n.name = {name} RETURN n" :: Query Person) (mkParams {name: "Serena"})`
 query :: forall eff a. (IsForeign a) => Query a -> Params -> InTransaction eff (Array a)
 query q params = do
   results <- privateRunQuery_ q params
   lift $ either liftError pure (traverse read results)
 
+-- | Like `query` with no parameters.
 query' :: forall eff a. (IsForeign a) => Query a -> InTransaction eff (Array a)
 query' q = query q (mkParams {})
 
@@ -141,9 +175,11 @@ privateRunQuery_ q (Params params) = do
   transaction <- ask
   lift $ makeAff (\reject accept -> runFn6 runQuery_ error reject accept transaction (show q) params)
 
+-- | Close the session.
 closeSession :: forall eff. Session -> Aff (db :: DB | eff) Unit
 closeSession session = liftEff $ runFn1 closeSession_ session
 
+-- | Close the driver.
 closeDriver :: forall eff. Driver -> Aff (db :: DB | eff) Unit
 closeDriver driver = liftEff $ runFn1 closeDriver_ driver
 
