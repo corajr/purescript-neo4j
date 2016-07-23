80ca9d5d0df131baeb02618846e68cf28b7f7a7a
Cora Johnson-Roberson <me@corajr.com>
2016-07-23 16:25:47 -0400

    example

--- example/Main.purs
+++ example/Main.purs
@@ -21,33 +21,36 @@ connectionInfo = ConnectionInfo
   }
 
 -- | Type for the database result.
-newtype Person = Person
-  { name :: String
+newtype Track = Track
+  { id :: Int
   , title :: String
   }
 
-derive instance genericPerson :: Generic Person
+derive instance genericTrack :: Generic Track
 
-instance isForeignPerson :: IsForeign Person where
-  read = readGeneric defaultForeignOpts
-
-instance eqPerson :: Eq Person where
+instance eqTrack :: Eq Track where
   eq = gEq
 
-instance showPerson :: Show Person where
+instance showTrack :: Show Track where
   show = gShow
 
--- | Add a record to the DB and return it.
-addToDB :: forall eff. Aff (neo4j :: NEO4J | eff) (Array Person)
-addToDB =
-  withDriver connectionInfo $ \driver ->
-    withSession driver $ \session ->
-      withRollback session $ do
-        execute' (Query "CREATE (a:Person {name:'Arthur', title:'King'})")
-        query (Query "MATCH (a:Person) WHERE a.name = {name} RETURN a" :: Query Person) (mkParams {name: "Arthur"})
+instance isForeignTrack :: IsForeign Track where
+  read = readGeneric defaultForeignOptions
 
-expected :: Array Person
-expected = [Person { name: "Arthur", title: "King"}]
+-- | Add a record to the DB and return it.
+addToDB :: forall eff. Session -> Aff (neo4j :: NEO4J | eff) (Array Track)
+addToDB session =
+  withRollback session $ do
+    execute (Query "CREATE (a:Track {id: {id}, title: {title}})") (mkParams {id: 999, title: "A test"})
+    query (Query "MATCH (a:Track) WHERE a.id = {id} RETURN a" :: Query Track) (mkParams {id: 999})
+
+readFromDB :: forall eff. Session -> Aff (neo4j :: NEO4J | eff) (Array Track)
+readFromDB session =
+  withTransaction session $
+    query (Query "MATCH (a:Track) WHERE a.id = {id} RETURN a" :: Query Track) (mkParams {id: 106521709})
+
+expected :: Array Track
+expected = [Track {id: 999, title: "A test"}]
 
 log' :: forall eff. String -> Aff (console :: CONSOLE | eff) Unit
 log' x = liftEff (log x)
@@ -59,16 +62,21 @@ logShow' x = do
 
 main :: Eff (console :: CONSOLE, err :: EXCEPTION, neo4j :: NEO4J) Unit
 main = void $ launchAff do
-  results <- attempt addToDB
-  case results of
-    Left err -> logShow' err *> pure unit
-    Right results' ->
-      if results' == expected
-      then log' "Ok"
-      else do
-        log' "Results did not match"
-        log' "Expected:"
-        logShow' expected
-        log' "Actual:"
-        logShow' results'
-        pure unit
+  withDriver connectionInfo $ \driver ->
+    withSession driver $ \session -> do
+      results <- attempt (addToDB session)
+      case results of
+        Left err -> logShow' err *> pure unit
+        Right results' ->
+          if results' == expected
+          then log' "Ok"
+          else do
+            log' "Results did not match"
+            log' "Expected:"
+            logShow' expected
+            log' "Actual:"
+            logShow' results'
+            pure unit
+      existing <- attempt (readFromDB session)
+      logShow' existing
+      pure unit
